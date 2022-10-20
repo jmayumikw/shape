8fe9a6086b975e11cf7fa134fbc495c889ea1a8b
jmayumikw <juliana.watanabe@enjoei.com.br>
2022-10-20 09:38:13 -0300

    add documentation and find_key test

--- app/main.py
+++ app/main.py
@@ -1,21 +1,41 @@
 
 import string
-from unittest import result
 from fastapi import FastAPI, Path, HTTPException
-app = FastAPI()
+
+tags_metadata = [
+    {
+        "name": "encode",
+        "description": "This endpoint encode a number and return a string",
+    },
+    {
+        "name": "decode",
+        "description": "This endpoint decode a string and return a number",
+    },
+]
+
+
+app = FastAPI(openapi_tags=tags_metadata)
 
 CHAR_LIST = string.ascii_uppercase + string.ascii_lowercase + string.digits + '!@#$%*()|_=+^/?'
 BASE = len(CHAR_LIST)
 DICT_CHAR = { CHAR_LIST[i]: i for i in range(len(CHAR_LIST))}
 
 def find_key(value):
+  """
+    This function find the key of value from a dictionary and returns a key
+  """
   for key, val in DICT_CHAR.items():
     if val == value:
       return key
+  raise TypeError("Only integers are allowed")
 
 @app.get("/encode/{num}")
 def encode(num: int = Path(title = "The number that will be encoded", ge = 0, le=99999999)):
+  """
+    This endpoint encode a number and return a string
+  """
   res_list = []
+  # use the same logic from binary encoding, but uses the DICT_CHAR
   while True:
     num, rest = divmod(num, BASE)
     res_list.append(find_key(rest))
@@ -25,9 +45,13 @@ def encode(num: int = Path(title = "The number that will be encoded", ge = 0, le
 
 @app.get("/decode/{code}")
 def decode(code: str):
+  """
+    This endpoint decode a string and return a number
+  """
   if len(code) != 6:
     raise HTTPException(status_code = 422, detail = "Invalid length")
   result = 0
+  # use the same logic for binary decoding to a number, but uses de DICT_CHAR
   for char in code:
     if char == "-":
       result = 0
--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -1,6 +1,8 @@
 from fastapi.testclient import TestClient
+import pytest
 
 from ..main import app
+from ..main import find_key
 
 client = TestClient(app)
 
@@ -27,3 +29,12 @@ def test_decode_with_invalid_input():
   response = client.get("/decode/&t&t$b")
   assert response.status_code == 422
   assert response.json() == { "detail": "Invalid input"}
+
+def test_find_key_with_success():
+  assert find_key(0) == "A"
+
+def test_find_key_with_invalid_input():
+  with pytest.raises(Exception) as response:
+    find_key("&")
+  assert "Only integers are allowed" in str(response.value)
+  assert response.type == TypeError
